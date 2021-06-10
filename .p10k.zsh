c4996c6b81bcc3a9597664417f99dc2d825a6901
xcad2k <lempa.christian@gmail.com>
2021-06-10 12:56:48 +0200

    update

--- .p10k.zsh
+++ .p10k.zsh
@@ -82,7 +82,7 @@
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     context                 # user@hostname
-    nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
+    # nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
     vim_shell               # vim shell indicator (:sh)
