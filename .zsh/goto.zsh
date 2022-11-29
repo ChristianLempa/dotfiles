4ee7e7c3116958a2d45c5f761af9c8efa1efa3f8
0xiDen <118856658+0xiDen@users.noreply.github.com>
2022-11-29 04:24:48 +0330

    Updated goto.zsh to support zsh beginning search
    One of my favorite features of zsh is history-beginning-search which can be done backward or forward in this commit I enabled this feature.

--- .zsh/goto.zsh
+++ .zsh/goto.zsh
@@ -1,4 +1,11 @@
 autoload -Uz compinit
 compinit
 
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
+
 source $(brew --prefix)/etc/bash_completion.d/goto.sh
