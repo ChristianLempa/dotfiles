c28578068459fdfa564f3575989ebf5d2eaa1bc7
Christian Lempa <christian.lempa@clcreative.de>
2023-10-15 09:36:21 +0200

    del goto

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+
+alias code="open -a 'Visual Studio Code'"
+
+alias gprj="cd $HOME/Projects"
+alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
+alias gbp="cd $HOME/Projects/christianlempa/boilerplates"
+
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias grep='grep --color'
+
