d348cbbea134e30c8fca0a4d9d895f7ad65efee3
Christian Lempa <christian.lempa@clcreative.de>
2023-10-15 09:28:14 +0200

    refreshed goto links

--- .zsh/wsl2fix.zsh
+++
@@ -1,8 +0,0 @@
-# Fix Interop Error that randomly occurs in vscode terminal when using WSL2
-fix_wsl2_interop() {
-    for i in $(pstree -np -s $$ | grep -o -E '[0-9]+'); do
-        if [[ -e "/run/WSL/${i}_interop" ]]; then
-            export WSL_INTEROP=/run/WSL/${i}_interop
-        fi
-    done
-}
